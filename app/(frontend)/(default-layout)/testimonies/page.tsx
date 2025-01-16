7662f904b5d40b2fa6e1d4d96158244b03787efd
Nathan <contact@nathanamick.com>
2025-01-16 06:04:55 -0500

    Add testimonies

---
+++ app/(frontend)/(default-layout)/testimonies/page.tsx
@@ -0,0 +1,39 @@
+import type { Metadata } from 'next/types'
+
+import configPromise from '@payload-config'
+import { getPayload } from 'payload'
+import React from 'react'
+import PostCategory from '../posts/post-category'
+
+export const dynamic = 'force-static'
+export const revalidate = 600
+
+export default async function Page() {
+  const payload = await getPayload({ config: configPromise })
+
+  const posts = await payload.find({
+    collection: 'posts',
+    depth: 1,
+    limit: 12,
+    overrideAccess: false,
+    select: {
+      title: true,
+      slug: true,
+      categories: true,
+      meta: true,
+    },
+    where: {
+      'categories.path': {
+        equals: 'testimonies',
+      },
+    },
+  })
+
+  return <PostCategory posts={posts} categoryName="Testimonies" />
+}
+
+export function generateMetadata(): Metadata {
+  return {
+    title: `Public Servants' Prayer Testimonies`,
+  }
+}

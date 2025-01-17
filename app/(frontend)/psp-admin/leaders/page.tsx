6a7c296ca9892f76aa4ae4bd3ff098c5dd9cd3f3
Nathan <contact@nathanamick.com>
2025-01-16 23:02:41 -0500

    Add leader missing images days away

--- app/(frontend)/psp-admin/leaders/page.tsx
+++ app/(frontend)/psp-admin/leaders/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import { stateCodes } from '@/data/states'
-import { getLeadersWithoutPhoto } from '@/lib/firebase/firestore'
+import { getOrderedLeadersForDailyPost } from '@/lib/firebase/firestore'
 import { Leader, StateCode } from '@/lib/types'
 import { getStateInfo } from '@/lib/get-state-info'
 import { CheckLeadersForPhotoButton } from './check-leader-images'
@@ -12,35 +12,60 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card'
+import { ScrollArea } from '@/components/ui/scroll-area'
+import { unstable_cache } from 'next/cache'
 
 export default async function LeadersPage() {
-  const stateLeaders = {} as Record<StateCode, Leader[]>
+  const stateLeaders = await unstable_cache(async () => {
+    const stateLeadersObj = {} as Record<
+      StateCode,
+      { leader: Leader; daysAway: number }[]
+    >
 
-  await Promise.all(
-    stateCodes.map(async (stateCode) => {
-      stateLeaders[stateCode] = await getLeadersWithoutPhoto(stateCode)
-    }),
-  )
+    await Promise.all(
+      stateCodes.map(async (stateCode) => {
+        const leaders = await getOrderedLeadersForDailyPost(stateCode)
+        stateLeadersObj[stateCode] = leaders
+          .map((leader, i) => {
+            const daysAway = Math.ceil((i + 1) / 3)
+            return {
+              leader,
+              daysAway,
+            }
+          })
+          .filter((leader) => leader.leader.hasPhoto === false)
+      }),
+    )
+
+    return stateLeadersObj
+  })()
 
   return (
     <div className="container">
       <h1 className="mb-4 text-xl">Leaders</h1>
 
       <div className="grid grid-cols-[1fr_auto] gap-4">
-        <div>
+        <div className="grid grid-cols-1 gap-4 lg:grid-cols-2 xl:grid-cols-3">
           {stateCodes.map((stateCode) => (
-            <div key={stateCode}>
-              <h2 className="mb-1 mt-2 text-sm">
-                {getStateInfo(stateCode).stateName}
-              </h2>
-              <ul>
-                {stateLeaders[stateCode].map((leader) => (
-                  <li key={leader.id} className="text-xs">
-                    {leader.FirstName} {leader.LastName} {leader.PhotoFile}
-                  </li>
-                ))}
-              </ul>
-            </div>
+            <Card key={stateCode} className="xxxmax-h-72 bg-card/40">
+              <CardHeader>
+                <h2 className="my-2 text-sm">
+                  {getStateInfo(stateCode).stateName}
+                </h2>
+              </CardHeader>
+
+              <CardContent>
+                <ScrollArea className="h-32">
+                  <ul>
+                    {stateLeaders[stateCode].map(({ leader, daysAway }) => (
+                      <li key={leader.id} className="text-xs">
+                        {leader.permaLink} - {daysAway} days away
+                      </li>
+                    ))}
+                  </ul>
+                </ScrollArea>
+              </CardContent>
+            </Card>
           ))}
         </div>
         <Card className="max-w-xs">
--- lib/firebase/firestore.ts
+++ lib/firebase/firestore.ts
@@ -58,6 +58,54 @@ export const getLeadersWithoutPhoto = async (stateCode: StateCode) => {
   return querySnapshot.docs.map((doc) => doc.data())
 }
 
+export const getOrderedLeadersForDailyPost = async (stateCode: StateCode) => {
+  console.log('getOrderedLeadersForDailyPost', stateCode)
+  const postSnapshot = await db
+    .collection('states')
+    .doc(stateCode)
+    .collection('posts')
+    .withConverter(PostConverter)
+    .orderBy('dateID', 'desc')
+    .limit(1)
+    .get()
+
+  if (postSnapshot.empty) {
+    throw new Error('No post found for state: ' + stateCode)
+  }
+
+  const leadersSnapshot = await db
+    .collection('states')
+    .doc(stateCode)
+    .collection('leaders')
+    .withConverter(LeaderConverter)
+    .orderBy('LastName')
+    .orderBy('PID')
+    .get()
+
+  if (leadersSnapshot.empty) {
+    throw new Error('No leaders found for state: ' + stateCode)
+  }
+
+  // rotate the leaders so that the post's leader3 is last and the rest are in order
+  const leaderDocs = leadersSnapshot.docs.map((doc) => doc.data())
+  const latestPost = postSnapshot.docs[0].data()
+  const leader3 = leaderDocs.find(
+    (leader) => leader.PID === latestPost.leader3.PID,
+  )
+  if (!leader3) {
+    throw new Error(
+      'No leader3 found for ' + stateCode + ' post: ' + latestPost.id,
+    )
+  }
+  const leader3Index = leaderDocs.indexOf(leader3)
+  const rotatedLeaders = [
+    ...leaderDocs.slice(leader3Index + 1),
+    ...leaderDocs.slice(0, leader3Index),
+  ]
+
+  return rotatedLeaders
+}
+
 export const getCollectionGroupLeaderByPermaLink = async (
   permaLink: string,
 ) => {

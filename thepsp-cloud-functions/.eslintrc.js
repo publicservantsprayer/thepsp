f70ed6791ff8d3ab646ac35faac1058112671303
Nathan <contact@nathanamick.com>
2025-01-30 15:43:24 -0500

    Add new cloud function typescript codebase

---
+++ thepsp-cloud-functions/.eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'google',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: ['tsconfig.json', 'tsconfig.dev.json'],
+    sourceType: 'module',
+  },
+  ignorePatterns: [
+    '/lib/**/*', // Ignore built files.
+    '/generated/**/*', // Ignore generated files.
+  ],
+  plugins: [
+    '@typescript-eslint',
+    'import',
+    'prettier', // Add this line
+  ],
+  rules: {
+    'import/no-unresolved': 0,
+    indent: ['error', 2],
+    'prettier/prettier': 'error', // Add this line
+  },
+}
